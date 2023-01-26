01bcf9a22687712f5cb84d86c30ceab8f9f71372
Juan Carlos Arriagada <109632032+jarriagadatorres@users.noreply.github.com>
2023-01-25 23:19:22 -0300

    actualización de despliegue de la lista y formato botones

--- assets/js/crud.js
+++ assets/js/crud.js
@@ -1,31 +1,34 @@
 function prueba() {
-    elementsTD = [
-        "Campo - RUT",
-        "Campoo - Nombre",
-        "Campo - Apellido"
-    ];
-    crearLinea(elementsTD);
-    elementsTD2 = [
-        "Campo 2 - RUT",
-        "Campo 2 - Nombre",
-        "Campo 2- Apellido"
-    ];
-    crearLinea(elementsTD2);
+    a_rut = ['Rut 1', 'Rut 2', 'Rut 3'];
+    a_nom = ['Nombre 1', 'Nombre 2', 'Nombre 3'];
+    a_app = ['Apellido 1', 'Apellido 2', 'Apellido 3'];
+    listar(a_rut, a_nom, a_app);
+    alert('Pausa');
+    a_rut =a_rut.concat(['Rut 4', 'Rut 5', 'Rut 6']);
+    a_nom = a_nom.concat(['Nombre 4', 'Nombre 5', 'Nombre 6']);
+    a_app = a_app.concat(['Apellido 4', 'Apellido 5', 'Apellido 6']);
+    listar(a_rut, a_nom, a_app);
 }
 
-function crearLinea(elements) {
-    const Lista_Nombres = document.querySelector('#Lista_Nombres');
+function crearLinea(elements, bandera,id) {
+    const Lista_Nombres = document.getElementById('Lista_Nombres');
     if (elements && Array.isArray(elements)) {
         tr = document.createElement('tr');
         Lista_Nombres.appendChild(tr);
         for (let index = 0; index < elements.length; index++) {
             const element = elements[index];
             const newtd = document.createElement('td');
+            if (bandera == 1) {
+                newtd.classList.add('bc_1');
+            } else {
+                newtd.classList.add('bc_2')
+            }
             newtd.textContent = element;
             Lista_Nombres.appendChild(newtd);
 
         }
         eliminarbtn = document.createElement('button');
+        eliminarbtn.onclick=() => eliminaElemento(id);
         eliminarbtn.textContent = 'Eliminar';
         eliminarbtn.classList.add('btn', 'btn-eliminar');
         Lista_Nombres.appendChild(eliminarbtn);
@@ -39,17 +42,19 @@ function crearLinea(elements) {
 }
 function listar(arr_rut, arr_nombre, arr_apellido) {      //recibo 3 arreglos obtenidos de Storage
     limpiarListaHtml();
+    bandera = 0
     for (i = 0; i < arr_rut.length; i++) {
         elementTD = [i, arr_rut[i], arr_nombre[i], arr_apellido[i]];
-        crearLinea(elementTD);
+        crearLinea(elementTD, bandera,i);
+        bandera = 1 - bandera;
     };
 }
 
 
 function limpiarListaHtml() {        //Limpia la lista HTML de la seccion <tbody> de Id: Lista_Nombres
-    const Lista_Nombres = Lista_Mombres.querySelector('#Lista_Nombres');
-    while (Lista__Nombres.firsthild) {
-        Lista_Nombres.removeChild(Lista_Nombres.firstChild);
+    const Lista_Nombres = document.getElementById('Lista_Nombres');
+    while (Lista_Nombres.firstElementChild) {
+        Lista_Nombres.removeChild(Lista_Nombres.firstElementChild);
     }
 }
 
@@ -66,7 +71,7 @@ function obtenerDatos(arr_rut, arr_nombre, arr_apellido) {   //Obtengo los datos
     /*
         agreagar datos del FORM en los arreglos
     */
-    listar(arr_rut, arr_nombre, arr_apellido);                // genero lista de datos
+    //listar(arr_rut, arr_nombre, arr_apellido);                // despliega lista de datos en HTML
     return;
 }
 
@@ -79,3 +84,12 @@ function GuardarDatos(arr_rut, arr_nombre, arr_apellido) { //almaceno los datos 
     */
     return;
 } 
+
+function eliminaElemento(id){
+    obtenerDatos(arr_rut, arr_nombre, arr_apellido)
+    remove = arr_rut.splice(id, 1);
+    remove = arr_nombre.splice(id, 1);
+    remove = arr_apellido.splice(id, 1);
+    GuardarDatos(arr_rut, arr_nombre, arr_apellido);
+    listar(arr_rut, arr_nombre, arr_apellido);
+}
