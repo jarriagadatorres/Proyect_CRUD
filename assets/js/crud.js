f545209ed8946272f8144c6a3650cc5a74ebd1bb
Juan Carlos Arriagada <109632032+jarriagadatorres@users.noreply.github.com>
2023-01-30 23:57:18 -0300

    Agreaga Formulario y corrije listar

--- assets/js/crud.js
+++ assets/js/crud.js
@@ -1,52 +1,87 @@
-function prueba() {
-    a_rut = ['Rut 1', 'Rut 2', 'Rut 3'];
-    a_nom = ['Nombre 1', 'Nombre 2', 'Nombre 3'];
-    a_app = ['Apellido 1', 'Apellido 2', 'Apellido 3'];
-    listar(a_rut, a_nom, a_app);
-    alert('Pausa');
-    a_rut =a_rut.concat(['Rut 4', 'Rut 5', 'Rut 6']);
-    a_nom = a_nom.concat(['Nombre 4', 'Nombre 5', 'Nombre 6']);
-    a_app = a_app.concat(['Apellido 4', 'Apellido 5', 'Apellido 6']);
-    listar(a_rut, a_nom, a_app);
+function inicia() {
+    if (localStorage.getItem('rut') == null) {
+        localStorage.setItem('lastId', 0)
+        localStorage.setItem('id', '[]');
+        localStorage.setItem('rut', '[]');
+        localStorage.setItem('nombre', '[]');
+        localStorage.setItem('apellido', '[]');
+    } else {
+        const Encabezado = document.getElementById('Encabezado');
+        tr = document.createElement('tr');
+        Encabezado.appendChild(tr);
+        th = document.createElement('th');
+        th.textContent = 'Id';
+        Encabezado.appendChild(th);
+        th = document.createElement('th');
+        th.textContent = 'RUT';
+        Encabezado.appendChild(th);
+        th = document.createElement('th');
+        th.textContent = 'Nombre';
+        Encabezado.appendChild(th);
+        th = document.createElement('th');
+        th.textContent = 'Apellido';
+        Encabezado.appendChild(th);
+        th = document.createElement('th');
+        Encabezado.appendChild(th);
+        th = document.createElement('th');
+        Encabezado.appendChild(th);
+
+        lastId = JSON.parse(localStorage.getItem('lastId'));
+        arr_id = JSON.parse(localStorage.getItem('id'));
+        arr_rut = JSON.parse(localStorage.getItem('rut'));
+        arr_nombre = JSON.parse(localStorage.getItem('nombre'));
+        arr_apellido = JSON.parse(localStorage.getItem('apellido'));
+        listar(arr_id, arr_rut, arr_nombre, arr_apellido);
+
+    }
+
+    return;
 }
 
-function crearLinea(elements, bandera,id) {
+function crearLinea(elements, idl) {
     const Lista_Nombres = document.getElementById('Lista_Nombres');
     if (elements && Array.isArray(elements)) {
         tr = document.createElement('tr');
+        tr.setAttribute("id", idl);
         Lista_Nombres.appendChild(tr);
+        const Lista_linea = document.getElementById(idl)
         for (let index = 0; index < elements.length; index++) {
+
             const element = elements[index];
             const newtd = document.createElement('td');
-            if (bandera == 1) {
-                newtd.classList.add('bc_1');
-            } else {
-                newtd.classList.add('bc_2')
-            }
             newtd.textContent = element;
-            Lista_Nombres.appendChild(newtd);
-
+            Lista_linea.appendChild(newtd);
         }
+
+        btnid1 = 'btn1'.concat(idl);
+        newtd = document.createElement('td');
+        newtd.setAttribute("id", btnid1);
+        Lista_linea.appendChild(newtd);
+        btn1 = document.getElementById(btnid1);
+        funcEliminar = '() => eliminaElemento('.concat(elements[0]).concat(')')
         eliminarbtn = document.createElement('button');
-        eliminarbtn.onclick=() => eliminaElemento(id);
+        eliminarbtn.onclick = () => eliminaElemento();
         eliminarbtn.textContent = 'Eliminar';
         eliminarbtn.classList.add('btn', 'btn-eliminar');
-        Lista_Nombres.appendChild(eliminarbtn);
+        btn1.appendChild(eliminarbtn);
+        newtd = document.createElement('td');
+        btnid2 = 'btn2'.concat(idl);
+        newtd.setAttribute("id", btnid2);
+        Lista_linea.appendChild(newtd);
+        btn2 = document.getElementById(btnid2);
         editarbtn = document.createElement('button');
         editarbtn.textContent = 'editar';
         editarbtn.classList.add('btn', 'btn-editar');
-        Lista_Nombres.appendChild(editarbtn);
+        btn2.appendChild(editarbtn);
 
     }
     return;
 }
-function listar(arr_rut, arr_nombre, arr_apellido) {      //recibo 3 arreglos obtenidos de Storage
+function listar(arr_id, arr_rut, arr_nombre, arr_apellido) {  //recibo 4 arreglos obtenidos de Storage
     limpiarListaHtml();
-    bandera = 0
     for (i = 0; i < arr_rut.length; i++) {
-        elementTD = [i, arr_rut[i], arr_nombre[i], arr_apellido[i]];
-        crearLinea(elementTD, bandera,i);
-        bandera = 1 - bandera;
+        elementTD = [arr_id[i], arr_rut[i], arr_nombre[i], arr_apellido[i]];
+        crearLinea(elementTD, arr_id[i]);
     };
 }
 
@@ -58,38 +93,78 @@ function limpiarListaHtml() {        //Limpia la lista HTML de la seccion <tbody
     }
 }
 
-function obtenerDatos(arr_rut, arr_nombre, arr_apellido) {   //Obtengo los datos desde el Storage que estan en 3 claves (rut, nombre, apellido)
-    if (localStorage.getItem('rut') == null) {
-        localStorage.setItem('rut', '');
-        localStorage.setItem('nombre', '');
-        localStorage.setItem('apellido', '');
-        return;
-    }
-    arr_rut = JASON.parse(localStorage.getItem('rut'));
-    arr_nombre = JASON.parse(localStorage.getItem('nombre'));
-    arr_apellido = JASON.parse(localStorage.getItem('apellido'));
-    /*
+/*function obtenerDatos(lastId, arr_id, arr_rut, arr_nombre, arr_apellido) {   //Obtengo los datos desde el Storage que estan en 4 claves (ID, rut, nombre, apellido)
+    lastId = JSON.parse(localStorage.getItem('lastId'));
+    arr_id = JSON.parse(localStorage.getItem('id'));
+    arr_rut = JSON.parse(localStorage.getItem('rut'));
+    arr_nombre = JSON.parse(localStorage.getItem('nombre'));
+    arr_apellido = JSON.parse(localStorage.getItem('apellido'));
+    //
         agreagar datos del FORM en los arreglos
-    */
+    //
     //listar(arr_rut, arr_nombre, arr_apellido);                // despliega lista de datos en HTML
     return;
-}
+}*/
 
-function GuardarDatos(arr_rut, arr_nombre, arr_apellido) { //almaceno los datos en el Storage en 3 claves (rut, nombre, apellido)
+function GuardarDatos(lastId, arr_id, arr_rut, arr_nombre, arr_apellido) { //almaceno los datos en el Storage en 3 claves (id, rut, nombre, apellido)
+    alert('entro en guardarDatos')
+    localStorage.setItem('lastId', JSON.stringify(lastId));
+    localStorage.setItem('id', JSON.stringify(arr_id));
     localStorage.setItem('rut', JSON.stringify(arr_rut));
     localStorage.setItem('nombre', JSON.stringify(arr_nombre));
     localStorage.setItem('apellido', JSON.stringify(arr_apellido));
     /* creo nueva linea en la lista
-        elementoTD=[id,formulario_rut,formulario_nombre_formularioo_apellido]
+        elementoTD=[id,formulario_rut,formulario_nombre,formularioo_apellido]
     */
     return;
-} 
+}
 
-function eliminaElemento(id){
-    obtenerDatos(arr_rut, arr_nombre, arr_apellido)
-    remove = arr_rut.splice(id, 1);
-    remove = arr_nombre.splice(id, 1);
-    remove = arr_apellido.splice(id, 1);
-    GuardarDatos(arr_rut, arr_nombre, arr_apellido);
-    listar(arr_rut, arr_nombre, arr_apellido);
+function eliminaElemento(i) {
+    let arr_id = JSON.parse(localStorage.getItem('id'));
+    let arr_rut = JSON.parse(localStorage.getItem('rut'));
+    let arr_nombre = JSON.parse(localStorage.getItem('nombre'));
+    let arr_apellido = JSON.parse(localStorage.getItem('apellido'));
+    let index = arr_id.indexOf(i);
+    remove = arr_id.splice(index, 1);
+    remove = arr_rut.splice(index, 1);
+    remove = arr_nombre.splice(index, 1);
+    remove = arr_apellido.splice(index, 1);
+    GuardarDatos(lastId, arr_id, arr_rut, arr_nombre, arr_apellido);
+    listar(arr_id, arr_rut, arr_nombre, arr_apellido);
+}
+
+function editaElemento(i) {
+    const formulario = document.getElementById('formulario')
+    let index = arr_id.indexOf(i);
+    arr_rut[index] = formulario.rut;
+    arr_nombre[index] = formulario.nombre;
+    arr_apellido[index] = formulario.apellido;
+    GuardarDatos(lastId, arr_id, arr_rut, arr_nombre, arr_apellido);
+    listar(arr_id, arr_rut, arr_nombre, arr_apellido);
+}
+
+function creaElemento() {
+
+    let lastId = JSON.parse(localStorage.getItem('lastId'));
+    let arr_rut = JSON.parse(localStorage.getItem('rut'));
+    let arr_id = JSON.parse(localStorage.getItem('id'));
+    let arr_nombre = JSON.parse(localStorage.getItem('nombre'));
+    let arr_apellido = JSON.parse(localStorage.getItem('apellido'));
+    i = ++lastId;
+    prut = document.getElementById('lrut').value;
+    arr_rut.push(prut);
+    pnombre = document.getElementById('lnombre').value;
+    arr_nombre.push(pnombre);
+    papellido = document.getElementById('lapellido').value;
+    arr_apellido.push(papellido);
+    GuardarDatos(lastId, arr_id, arr_rut, arr_nombre, arr_apellido);
+    listar(arr_id, arr_rut, arr_nombre, arr_apellido);
+}
+
+function cancelaIngreso() {
+    const formulario = document.getElementById('formulario');
+    formulario.rut = '';
+    formulario.nombre = '';
+    formulario.apellido = '';
+
 }
