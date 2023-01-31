ed0e18658d83f965fb76ce1191320749339aa88e
Juan Carlos Arriagada <109632032+jarriagadatorres@users.noreply.github.com>
2023-01-31 20:04:26 -0300

    Version con activacion de botones Eliminar y Editar de la tabla

--- assets/js/crud.js
+++ assets/js/crud.js
@@ -7,8 +7,6 @@ function inicia() {
         localStorage.setItem('apellido', '[]');
     } else {
         const Encabezado = document.getElementById('Encabezado');
-        tr = document.createElement('tr');
-        Encabezado.appendChild(tr);
         th = document.createElement('th');
         th.textContent = 'Id';
         Encabezado.appendChild(th);
@@ -25,16 +23,9 @@ function inicia() {
         Encabezado.appendChild(th);
         th = document.createElement('th');
         Encabezado.appendChild(th);
-
-        lastId = JSON.parse(localStorage.getItem('lastId'));
-        arr_id = JSON.parse(localStorage.getItem('id'));
-        arr_rut = JSON.parse(localStorage.getItem('rut'));
-        arr_nombre = JSON.parse(localStorage.getItem('nombre'));
-        arr_apellido = JSON.parse(localStorage.getItem('apellido'));
-        listar(arr_id, arr_rut, arr_nombre, arr_apellido);
+        listar();
 
     }
-
     return;
 }
 
@@ -52,119 +43,132 @@ function crearLinea(elements, idl) {
             newtd.textContent = element;
             Lista_linea.appendChild(newtd);
         }
-
-        btnid1 = 'btn1'.concat(idl);
-        newtd = document.createElement('td');
-        newtd.setAttribute("id", btnid1);
-        Lista_linea.appendChild(newtd);
-        btn1 = document.getElementById(btnid1);
-        funcEliminar = '() => eliminaElemento('.concat(elements[0]).concat(')')
-        eliminarbtn = document.createElement('button');
-        eliminarbtn.onclick = () => eliminaElemento();
-        eliminarbtn.textContent = 'Eliminar';
-        eliminarbtn.classList.add('btn', 'btn-eliminar');
-        btn1.appendChild(eliminarbtn);
-        newtd = document.createElement('td');
-        btnid2 = 'btn2'.concat(idl);
-        newtd.setAttribute("id", btnid2);
-        Lista_linea.appendChild(newtd);
-        btn2 = document.getElementById(btnid2);
-        editarbtn = document.createElement('button');
-        editarbtn.textContent = 'editar';
-        editarbtn.classList.add('btn', 'btn-editar');
-        btn2.appendChild(editarbtn);
-
+        document.getElementById(idl).innerHTML +=
+            `<td>
+            <button class="btn btn-eliminar" onclick="eliminaElemento('${idl}')">eliminar</button>
+            </td>
+            <td>
+            <button class="btn btn-editar" onclick="editaElemento('${idl}')">editar</button>
+            </td>`
     }
     return;
 }
-function listar(arr_id, arr_rut, arr_nombre, arr_apellido) {  //recibo 4 arreglos obtenidos de Storage
+function listar() {  //recibo 4 arreglos obtenidos de Storage
+    lastId = JSON.parse(localStorage.getItem('lastId'));
+    arr_id = JSON.parse(localStorage.getItem('id'));
+    arr_rut = JSON.parse(localStorage.getItem('rut'));
+    arr_nombre = JSON.parse(localStorage.getItem('nombre'));
+    arr_apellido = JSON.parse(localStorage.getItem('apellido'))
     limpiarListaHtml();
     for (i = 0; i < arr_rut.length; i++) {
         elementTD = [arr_id[i], arr_rut[i], arr_nombre[i], arr_apellido[i]];
         crearLinea(elementTD, arr_id[i]);
     };
+    document.getElementById('formulario').innerHTML = ` 
+    <input type="number" id="lid" value=0 hidden >
+    <label for="rut">RUT : <input type="text" id="lrut" name="lrut" value=""
+        required minlength="8" maxlength="8" size="10"></label>
+    <label for="nombre">Nombre : <input type="text" id="lnombre" name="lnombre" value=""
+        required size="20"></label>
+    <label for="apellido">Apellido : <input type="text" id="lapellido"  name="lapellido" value=""
+        required size="20"></label>
+    <button class="btn btn-guardar" onclick="actualizaElemento()">Guardar</button>
+    <button class="btn btn-eliminar" onclick="listar()">Cancelar</button>`
 }
 
 
-function limpiarListaHtml() {        //Limpia la lista HTML de la seccion <tbody> de Id: Lista_Nombres
+function limpiarListaHtml() {               //Limpia la lista HTML de la seccion <tbody> de Id: Lista_Nombres
     const Lista_Nombres = document.getElementById('Lista_Nombres');
     while (Lista_Nombres.firstElementChild) {
         Lista_Nombres.removeChild(Lista_Nombres.firstElementChild);
     }
 }
 
-/*function obtenerDatos(lastId, arr_id, arr_rut, arr_nombre, arr_apellido) {   //Obtengo los datos desde el Storage que estan en 4 claves (ID, rut, nombre, apellido)
-    lastId = JSON.parse(localStorage.getItem('lastId'));
-    arr_id = JSON.parse(localStorage.getItem('id'));
-    arr_rut = JSON.parse(localStorage.getItem('rut'));
-    arr_nombre = JSON.parse(localStorage.getItem('nombre'));
-    arr_apellido = JSON.parse(localStorage.getItem('apellido'));
-    //
-        agreagar datos del FORM en los arreglos
-    //
-    //listar(arr_rut, arr_nombre, arr_apellido);                // despliega lista de datos en HTML
-    return;
-}*/
+function actualizaElemento(i) {             //actualiso los datos en el Storage con 4 claves (id, rut, nombre, apellido)
+    let arr_id = JSON.parse(localStorage.getItem('id'));
+    let arr_rut = JSON.parse(localStorage.getItem('rut'));
+    let arr_nombre = JSON.parse(localStorage.getItem('nombre'));
+    let arr_apellido = JSON.parse(localStorage.getItem('apellido'));
 
-function GuardarDatos(lastId, arr_id, arr_rut, arr_nombre, arr_apellido) { //almaceno los datos en el Storage en 3 claves (id, rut, nombre, apellido)
-    alert('entro en guardarDatos')
+    prut = document.getElementById('lrut').value;
+    papellido = document.getElementById('lapellido').value;
+    pnombre = document.getElementById('lnombre').value;
+
+    let index = arr_id.indexOf(parseInt(i));
+
+    arr_rut[index] = prut;
+    arr_nombre[index] = pnombre;
+    arr_apellido[index] = papellido;
+
     localStorage.setItem('lastId', JSON.stringify(lastId));
     localStorage.setItem('id', JSON.stringify(arr_id));
     localStorage.setItem('rut', JSON.stringify(arr_rut));
     localStorage.setItem('nombre', JSON.stringify(arr_nombre));
     localStorage.setItem('apellido', JSON.stringify(arr_apellido));
-    /* creo nueva linea en la lista
-        elementoTD=[id,formulario_rut,formulario_nombre,formularioo_apellido]
-    */
+    return;
+}
+
+function GuardarDatos(lastId, arr_id, arr_rut, arr_nombre, arr_apellido) { //almaceno los datos en el Storage con 5 claves (lastId, id, rut, nombre, apellido)
+    localStorage.setItem('lastId', JSON.stringify(lastId));
+    localStorage.setItem('id', JSON.stringify(arr_id));
+    localStorage.setItem('rut', JSON.stringify(arr_rut));
+    localStorage.setItem('nombre', JSON.stringify(arr_nombre));
+    localStorage.setItem('apellido', JSON.stringify(arr_apellido));
     return;
 }
 
 function eliminaElemento(i) {
+    let lastId = JSON.parse(localStorage.getItem('lastId'));
     let arr_id = JSON.parse(localStorage.getItem('id'));
     let arr_rut = JSON.parse(localStorage.getItem('rut'));
     let arr_nombre = JSON.parse(localStorage.getItem('nombre'));
     let arr_apellido = JSON.parse(localStorage.getItem('apellido'));
-    let index = arr_id.indexOf(i);
+    let index = arr_id.indexOf(parseInt(i));
     remove = arr_id.splice(index, 1);
     remove = arr_rut.splice(index, 1);
     remove = arr_nombre.splice(index, 1);
     remove = arr_apellido.splice(index, 1);
     GuardarDatos(lastId, arr_id, arr_rut, arr_nombre, arr_apellido);
-    listar(arr_id, arr_rut, arr_nombre, arr_apellido);
+    listar();
 }
 
 function editaElemento(i) {
-    const formulario = document.getElementById('formulario')
-    let index = arr_id.indexOf(i);
-    arr_rut[index] = formulario.rut;
-    arr_nombre[index] = formulario.nombre;
-    arr_apellido[index] = formulario.apellido;
-    GuardarDatos(lastId, arr_id, arr_rut, arr_nombre, arr_apellido);
-    listar(arr_id, arr_rut, arr_nombre, arr_apellido);
+    let arr_id = JSON.parse(localStorage.getItem('id'));
+    let arr_rut = JSON.parse(localStorage.getItem('rut'));
+    let arr_nombre = JSON.parse(localStorage.getItem('nombre'));
+    let arr_apellido = JSON.parse(localStorage.getItem('apellido'));
+    let index = arr_id.indexOf(parseInt(i));
+    document.getElementById('formulario').innerHTML = `
+        <input type="number" id="lid" value=${arr_id[index]} hidden >
+        <label for="rut">RUT : <input type="text" id="lrut" name="lrut" value="${arr_rut[index]}"
+            required minlength="8" maxlength="8" size="10"></label>
+        <label for="nombre">Nombre : <input type="text" id="lnombre" name="lnombre" value="${arr_nombre[index]}"
+            required size="20"></label>
+        <label for="apellido">Apellido : <input type="text" id="lapellido"  name="lapellido" value="${arr_apellido[index]}"
+            required size="20"></label>
+        <button class="btn btn-guardar" onclick="actualizaElemento(${arr_id[index]})">Actualiza</button>
+        <button class="btn btn-eliminar" onclick="listar()">Cancelar</button>`
 }
 
 function creaElemento() {
 
     let lastId = JSON.parse(localStorage.getItem('lastId'));
-    let arr_rut = JSON.parse(localStorage.getItem('rut'));
     let arr_id = JSON.parse(localStorage.getItem('id'));
+    let arr_rut = JSON.parse(localStorage.getItem('rut'));
     let arr_nombre = JSON.parse(localStorage.getItem('nombre'));
     let arr_apellido = JSON.parse(localStorage.getItem('apellido'));
     i = ++lastId;
+
     prut = document.getElementById('lrut').value;
-    arr_rut.push(prut);
-    pnombre = document.getElementById('lnombre').value;
-    arr_nombre.push(pnombre);
     papellido = document.getElementById('lapellido').value;
+    pnombre = document.getElementById('lnombre').value;
+
+    arr_rut.push(prut);
+    arr_nombre.push(pnombre);
     arr_apellido.push(papellido);
+    arr_id.push(i);
     GuardarDatos(lastId, arr_id, arr_rut, arr_nombre, arr_apellido);
-    listar(arr_id, arr_rut, arr_nombre, arr_apellido);
+    listar();
 }
 
-function cancelaIngreso() {
-    const formulario = document.getElementById('formulario');
-    formulario.rut = '';
-    formulario.nombre = '';
-    formulario.apellido = '';
 
-}
